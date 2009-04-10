b7a1a53bc9f8de1215e6609783c673ad14079421
justin.cherniak <justin.cherniak@90da9a02-2610-11de-a9f6-2f640129b477>
2009-04-10 21:01:04 +0000

    Initial commit.  Supports still image capture

---
+++ src/webcam-capture/CameraForm.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace WebCamCapture
+{
+	partial class CameraForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.m_pictureBox = new System.Windows.Forms.PictureBox();
+			this.m_btnTakePicture = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.m_pictureBox)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// m_pictureBox
+			// 
+			this.m_pictureBox.Location = new System.Drawing.Point(12, 12);
+			this.m_pictureBox.Name = "m_pictureBox";
+			this.m_pictureBox.Size = new System.Drawing.Size(320, 240);
+			this.m_pictureBox.TabIndex = 0;
+			this.m_pictureBox.TabStop = false;
+			// 
+			// m_btnTakePicture
+			// 
+			this.m_btnTakePicture.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.m_btnTakePicture.Location = new System.Drawing.Point(66, 258);
+			this.m_btnTakePicture.Name = "m_btnTakePicture";
+			this.m_btnTakePicture.Size = new System.Drawing.Size(206, 48);
+			this.m_btnTakePicture.TabIndex = 1;
+			this.m_btnTakePicture.Text = "&Take Picture";
+			this.m_btnTakePicture.UseVisualStyleBackColor = true;
+			this.m_btnTakePicture.Click += new System.EventHandler(this.OnTakePictureClick);
+			// 
+			// CameraForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(348, 318);
+			this.Controls.Add(this.m_btnTakePicture);
+			this.Controls.Add(this.m_pictureBox);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "CameraForm";
+			this.ShowIcon = false;
+			this.ShowInTaskbar = false;
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+			this.Text = "Camera";
+			((System.ComponentModel.ISupportInitialize)(this.m_pictureBox)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox m_pictureBox;
+		private System.Windows.Forms.Button m_btnTakePicture;
+	}
+}
---
+++ src/webcam-capture/CameraForm.cs
@@ -0,0 +1,72 @@
+﻿// Webcam-Capture - A simple library for capturing images from a webcam
+//
+// Copyright © 2009 Justin Cherniak
+//
+// This library is free software; you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation; either version 2.1 of the License, or (at
+// your option) any later version.
+//
+// This library is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+// License (COPYING.txt) for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this library; if not, write to the Free Software Foundation,
+// Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WebCamCapture
+{
+	public partial class CameraForm : Form
+	{
+		private WebCam m_webCam;
+		private Image m_image;
+
+		public CameraForm()
+		{
+			Cursor.Current = Cursors.WaitCursor;
+			InitializeComponent();
+
+			m_webCam = new WebCam(0, m_pictureBox.Handle, 320, 240, true);
+
+			Cursor.Current = Cursors.Default;
+		}
+
+		private void OnTakePictureClick(object sender, EventArgs e)
+		{
+			m_image = m_webCam.TakePicture();
+			m_webCam.Stop();
+
+			DialogResult = DialogResult.OK;
+			this.Close();
+		}
+
+		public static Image TakePicture()
+		{
+			CameraForm form = new CameraForm();
+			form.ShowDialog();
+
+			return form.m_image;
+		}
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+				m_webCam.Dispose();
+				if (components != null)
+					components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+	}
+}
